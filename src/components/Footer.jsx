0d4da07e9e413d4083ef55872ac79d07be411466
millie <clickkm24@naver.com>
2026-05-25 13:51:49 +0900

    footer 문구 변경

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 function Footer() {
   return (
     <footer className={styles.footer}>
-      © 2026 계산기앱 · 한 손으로 가볍게
+      © 2026 담토리 (Dam Tally) by millie
     </footer>
   )
 }
